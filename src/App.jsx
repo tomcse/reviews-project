fe181fea6f7df10419966e17ce3d83fffc46e9e7
Tommy <tommychoa@outlook.com>
2023-07-19 15:01:10 +0800

    completed proj

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,77 @@
+import { useState } from "react";
+import reviewsData from "./data";
+import { FaQuoteLeft, FaChevronLeft, FaChevronRight } from "react-icons/fa";
+
 const App = () => {
-  return <h2>Reviews Starter</h2>;
+  const [index, setIndex] = useState(0);
+  const { image, job, name, text } = reviewsData[index];
+
+  const nextBtn = () => {
+    if (index === reviewsData.length - 1) {
+      setIndex(0);
+    } else {
+      setIndex((currentIndex) => {
+        const newIndex = currentIndex + 1;
+        return newIndex;
+      });
+    }
+  };
+
+  const prevBtn = () => {
+    if (index === 0) {
+      setIndex(3);
+    } else {
+      setIndex((currentIndex) => {
+        const newIndex = currentIndex - 1;
+        return newIndex;
+      });
+    }
+  };
+
+  const randomReview = () => {
+    setIndex(() => {
+      const randomNumber = Math.floor(Math.random() * reviewsData.length);
+      if (randomNumber === index) {
+        if (index === reviewsData.length - 1) {
+          return 0;
+        } else {
+          return randomNumber + 1;
+        }
+      } else {
+        return randomNumber;
+      }
+    });
+  };
+
+  return (
+    <main>
+      <article className="review">
+        <div className="img-container">
+          <img src={image} alt={name} className="person-img" />
+          <div className="quote-icon">
+            <FaQuoteLeft />
+          </div>
+        </div>
+        <h4 className="author">{name}</h4>
+        <p className="job">{job}</p>
+        <p className="info">{text}</p>
+        <div className="btn-container">
+          <button type="button" className="prev-btn" onClick={prevBtn}>
+            <FaChevronLeft />
+          </button>
+          <button type="button" className="next-btn" onClick={nextBtn}>
+            <FaChevronRight />
+          </button>
+        </div>
+        <button
+          type="button"
+          className="btn btn-hipster"
+          onClick={randomReview}
+        >
+          surprise me
+        </button>
+      </article>
+    </main>
+  );
 };
 export default App;
